d4e7997329176ce957704d560370fa888f6802d8
Denis Po <denis909@mail.ru>
2020-10-01 11:52:39 +0300

    n/a

--- Controller.php
+++ Controller.php
@@ -6,76 +6,15 @@ use Yii;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use yii\helpers\Url;
-use yii\web\NotFoundHttpException;
-use denis909\yii\Assert;
-use yii\base\Model;
 
 class Controller extends \yii\web\Controller
 {
 
     public $userComponent = 'user';
 
-    public $modelClass;
-
     public $postActions = [];
 
     public $roles = [];
-
-    public function throwNotFoundHttpException($message = 'Page not found.', $class = NotFoundHttpException::class)
-    {
-        throw Yii::createObject($class, [$message]);
-    }
-
-    public function findModel($id, $modelClass = null)
-    {
-        if (!$modelClass)
-        {
-            $modelClass = $this->modelClass;
-        }
-
-        Assert::notEmpty($modelClass);
-
-        Assert::notEmpty($id);
-            
-        $model = $modelClass::findOne($id);
-        
-        if (!$model)
-        {
-            $this->throwNotFoundHttpException();
-        }
-
-        return $model;
-    }
-
-    public function createModel(string $className)
-    {
-        return $model = Yii::createObject($className);
-    }
-
-    public function createModelQuery(string $className)
-    {
-        return $className::find();
-    }    
-
-    public function loadModel(Model $model, array $data = [])
-    {
-        return $model->load($data);
-    }
-
-    public function saveModel(Model $model, bool $validate = true, array $attributes = null)
-    {
-        return $model->save($validate, $attributes);
-    }
-
-    public function validateModel(Model $model, $attributes = null)
-    {
-        return $model->validate($attributes);
-    }
-
-    public function deleteModel(Model $model)
-    {
-        return $model->delete();
-    }
 
     public function redirectBack($default = null)
     {
@@ -131,9 +70,9 @@ class Controller extends \yii\web\Controller
         if (array_key_exists('access', $return))
         {
             $return['access']['class'] = AccessControl::class;
-
-            $return['access']['user'] = $this->userComponent;
         }
+
+        $return['access']['user'] = $this->userComponent;
 
         return $return;
     }
